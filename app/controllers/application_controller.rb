8196c39798f6a702f7095847b5c9513d928a3e49
Unknown <siriiven@gmail.com>
2018-10-11 15:30:44 -0400

    Login Page
    Login Page Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-layout 'main/layout-2'
+  layout :layout
 
    private
    def layout
      # only turn it off for login pages:
-     is_a?(Devise::SessionsController) ? false : "application"
+     is_a?(Devise::SessionsController) ? false : "main/layout-2"
      # or turn layout off for every devise controller:
      # devise_controller? && "application"
    end
